8688d93294f30a1f881b9593e89357b8c00005d9
Şeyma Gökhan <40568529+symgkhn@users.noreply.github.com>
2021-03-19 20:16:12 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine( musteri.Ad +" "+ musteri.Soyad + " " +musteri.Id +" " + "Müşteri Eklendi" );
+        }
+
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine( musteri.Id +" " + "Id'li müşteri silindi  " );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Şeyma";
+            musteri1.Soyad = "Gökhan";
+            musteri1.Id = 1234;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Büşra";
+            musteri2.Soyad = "Cenk";
+            musteri2.Id = 1223;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Kübra";
+            musteri3.Soyad = "Çamlı";
+            musteri3.Id = 4567;
+
+            Musteri[] musteriler = new Musteri[] { musteri1,musteri2,musteri3};
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " " + musteri.Id);
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriSilme(musteri1);
+
+
+        }
+    }
+}
